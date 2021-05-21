b6d1770ba11f21839cd9a445fef460b5dc237ffd
yuxuan.dong <yuxuan.dong@ucloud.cn>
2021-05-21 11:26:09 +0800

    fix

--- Cask/robot-3t.rb
+++ Cask/robot-3t.rb
@@ -1,17 +1,17 @@
 cask 'robo-3t' do
     version '1.2.1,3e50a65'
-      sha256 'd57940bf81ee812bd49d2b9d7468f33c9a9d39004a0867cef5465ffb6a6d3844'
+    sha256 'd57940bf81ee812bd49d2b9d7468f33c9a9d39004a0867cef5465ffb6a6d3844'
 
-        url "https://github.com/Studio3T/robomongo/releases/download/v#{version.before_comma}/robo3t-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
-          name 'Robo 3T (formerly Robomongo)'
-            homepage 'https://robomongo.org/'
+    url "https://github.com/Studio3T/robomongo/releases/download/v#{version.before_comma}/robo3t-#{version.before_comma}-darwin-x86_64-#{version.after_comma}.dmg"
+    name 'Robo 3T (formerly Robomongo)'
+    homepage 'https://robomongo.org/'
 
-              app 'Robo 3T.app'
+    app 'Robo 3T.app'
 
-                uninstall quit: 'Robo 3T'
+    uninstall quit: 'Robo 3T'
 
-                  zap trash: [
-                                   '~/.3T/robo-3t/',
-                                                  '~/Library/Saved Application State/Robo 3T.savedState',
-                                                               ]
+    zap trash: [
+        '~/.3T/robo-3t/',
+        '~/Library/Saved Application State/Robo 3T.savedState',
+    ]
 end
